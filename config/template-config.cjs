c645398afb4dfe07ae74d912730f131c51c77cd2
yilin-gong <78230512+yilin-gong@users.noreply.github.com>
2023-07-24 16:49:55 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -29,15 +29,9 @@ const TEMPLATE_CONFIG = [
     desc: `
       服务器信息：{{post_time_zone.DATA}} {{post_time.DATA}}
       
-      ---
-      
       共推送 {{need_post_num.DATA}} 人
       
-      成功: {{success_post_num.DATA}} | 失败: {{fail_post_num.DATA}}
-      
       成功用户: {{success_post_ids.DATA}}
-      
-      失败用户: {{fail_post_ids.DATA}}
     `
   },
 ]
