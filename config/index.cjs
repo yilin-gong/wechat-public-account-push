49783646d3776ca75c05d31b463b97ee3a446692
yilin-gong <78230512+yilin-gong@users.noreply.github.com>
2023-07-24 14:29:33 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,24 +8,23 @@
 const USER_CONFIG = {
   
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
-  // 请将以下代码按照图示粘贴到对应位置
-  USE_PASSAGE: 'push-deer',
+  APP_ID: 'wx1a40775b8cf23543',
+
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: '8f71be8615d5eed8912f1084d91a5e4b',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '北京',
+  CITY: '北京',
 
   USERS: [
     {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'PDU24305T5WV5EEkgOGfUt790rqp6uCQqXIwAuGiK',
+      id: 'obapD6YN4t0AE-YvcFv5UVEVyEd0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '001',
+      useTemplateId: 'NnW68Lt-iQMM-zhOfQHt3zz-BTgagcaMQqwLngnQKQ4',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '07-23',
       festivals: [
@@ -53,7 +52,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'obapD6YN4t0AE-YvcFv5UVEVyEd0',
     }
   ],
 
